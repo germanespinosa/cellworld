de2b24c686b52dff9ce849efc34eda1e928a1721
German Espinosa <germanespinosa@gmail.com>
2021-10-28 14:35:37 -0500

    fix where

--- include/cell_world/map.h
+++ include/cell_world/map.h
@@ -8,7 +8,7 @@ namespace cell_world{
         explicit Map(const Cell_group &);
         const Cell& operator [](const Coordinates &) const;
         int find (const Coordinates &) const;
-        const Cell_group cells;
+        Cell_group cells;
         Coordinates_list coordinates;
     private:
         std::vector<int> _coordinate_index;
