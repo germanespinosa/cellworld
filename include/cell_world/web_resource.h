38942add027356013fa68e64e371afdc61fc0359
German Espinosa <germanespinosa@gmail.com>
2021-10-29 14:13:04 -0500

    fix where

--- include/cell_world/web_resource.h
+++ include/cell_world/web_resource.h
@@ -4,10 +4,16 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <json_cpp.h>
+
 namespace cell_world {
     struct Web_resource {
         static std::string &cache_folder();
         static Web_resource from(const std::string);
+        template <class T>
+        T get_resource (){
+            return json_cpp::Json_create<T>(get());
+        }
         Web_resource &key(const std::string &);
         Web_resource &key(int);
         Web_resource &key(unsigned int);
