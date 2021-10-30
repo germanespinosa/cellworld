d58d050d07eb0d1f6d4ae9bd9188328fed77be47
German Espinosa <germanespinosa@gmail.com>
2021-10-30 12:20:26 -0500

    added experiment step

---
+++ include/cell_world/experiment.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <cell_world/core.h>
+#include <cell_world/cell_group.h>
+
+namespace cell_world {
+    struct Step : json_cpp::Json_object{
+        Json_object_members(
+                Add_member(time_stamp);
+                Add_member(agent_name);
+                Add_member(frame);
+                Add_member(coordinates);
+                Add_member(location);
+                Add_member(rotation);
+        );
+        double time_stamp;
+        std::string agent_name;
+        unsigned int frame;
+        cell_world::Coordinates coordinates;
+        cell_world::Location location;
+        double rotation;
+    };
+}
