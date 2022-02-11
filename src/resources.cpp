4e8c68d049cd1bca6121e0b6ff0e05433df8a9ad
German Espinosa <germanespinosa@gmail.com>
2022-02-11 12:22:50 -0600

    Convert step

--- src/resources.cpp
+++ src/resources.cpp
@@ -46,6 +46,8 @@ namespace cell_world {
         }
         { // time to download the resource
             auto uri = url() + _cache_invalidation();
+            auto json_uri = json_cpp::Json_URI(uri);
+            auto req = json_cpp::Json_web_request(json_uri).get_response();
             auto wr = Json_web_get(uri);
             ofstream cache_file;
             cache_file.open(cfn);
