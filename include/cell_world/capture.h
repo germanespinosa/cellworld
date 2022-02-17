df39a3a02bfb029726a1f58cc51be88ac30dc323
German Espinosa <germanespinosa@gmail.com>
2022-02-17 16:16:55 -0600

    added peeking

--- include/cell_world/capture.h
+++ include/cell_world/capture.h
@@ -2,6 +2,7 @@
 #include <cell_world/core.h>
 #include <cell_world/world.h>
 #include <cell_world/visibility.h>
+#include <cell_world/timer.h>
 
 namespace cell_world {
 
@@ -10,8 +11,16 @@ namespace cell_world {
                 Add_member(margin);
                 Add_member(time_limit);
         );
-        double margin;
-        double time_limit;
+        double margin{};
+        double time_limit{};
+    };
+
+    struct Peeking {
+        Peeking(const Peeking_parameters &, const World &);
+        Location_visibility peeking_visibility;
+        Timer peeking_timer;
+        bool is_seen(const Location &predator_location, const Location &prey_location);
+        void not_visible();
     };
 
     struct Capture_parameters : json_cpp::Json_object {
@@ -28,6 +37,6 @@ namespace cell_world {
         double dist_max;
         double theta_diff_max;
         Location_visibility visibility;
-        bool is_captured( const Location &predator_location, double predator_theta, const Location &prey_location);
+        bool is_captured(const Location &predator_location, double predator_theta, const Location &prey_location);
     };
 }
--- src/capture.cpp
+++ src/capture.cpp
@@ -5,12 +5,32 @@ namespace cell_world {
     Capture::Capture(const Capture_parameters &parameters, const World &world):
             dist_max (parameters.distance * world.cell_transformation.size),
             theta_diff_max(to_radians(parameters.angle / 2)),
-            visibility(Location_visibility(world.create_cell_group() ,world.cell_shape, world.cell_transformation)){
+            visibility(world.create_cell_group() ,world.cell_shape, world.cell_transformation){
     }
 
     bool Capture::is_captured(const Location &predator_location, double predator_theta, const Location &prey_location) {
         if (predator_location.dist(prey_location) > dist_max) return false;
         if (angle_difference(predator_location.atan(prey_location),predator_theta) > theta_diff_max) return false;
         return visibility.is_visible(predator_location, prey_location) ;
+    }
+
+    Peeking::Peeking(const Peeking_parameters &parameters, const World &world):
+    peeking_visibility(world.create_cell_group() ,world.cell_shape, Transformation((1.0 + parameters.margin) * world.cell_transformation.size,world.cell_transformation.rotation)),
+    peeking_timer(parameters.time_limit) {
+
+    }
+
+    void Peeking::not_visible() {
+        peeking_timer.reset();
+    }
+
+    bool Peeking::is_seen(const Location &predator_location, const Location &prey_location) {
+        if (peeking_visibility.is_visible(predator_location, prey_location)){
+            return true;
+        }
+        if (peeking_timer.time_out()) {
+            return true;
+        }
+        return false;
     }
 }
