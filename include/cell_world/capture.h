141580c31c47811cd85b8d5230ec9b4cd0f65d7f
German Espinosa <germanespinosa@gmail.com>
2022-02-17 12:23:46 -0600

    added captures

---
+++ include/cell_world/capture.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <cell_world/core.h>
+#include <cell_world/world.h>
+#include <cell_world/visibility.h>
+
+namespace cell_world {
+    struct Capture_parameters : json_cpp::Json_object {
+        Json_object_members(
+                Add_member(distance);
+                Add_member(angle);
+        );
+        double distance{};
+        double angle{};
+    };
+
+    struct Capture {
+        Capture(const Capture_parameters &, const World &);
+        double dist_max;
+        double theta_diff_max;
+        Location_visibility visibility;
+        bool is_captured( const Location &predator_location, double predator_theta, const Location &prey_location);
+    };
+}
---
+++ src/capture.cpp
@@ -0,0 +1,16 @@
+#include <cell_world/capture.h>
+
+namespace cell_world {
+
+    Capture::Capture(const Capture_parameters &parameters, const World &world):
+            dist_max (parameters.distance * world.cell_transformation.size),
+            theta_diff_max(to_radians(parameters.angle / 2)),
+            visibility(Location_visibility(world.create_cell_group() ,world.cell_shape, world.cell_transformation)){
+    }
+
+    bool Capture::is_captured(const Location &predator_location, double predator_theta, const Location &prey_location) {
+        if (predator_location.dist(prey_location) > dist_max) return false;
+        if (abs(predator_location.atan(prey_location) - predator_theta) > theta_diff_max) return false;
+        return visibility.is_visible(predator_location, prey_location) ;
+    }
+}
