50f52015bfa267f2b8cfde3b9ac098056b627ac0
German Espinosa <germanespinosa@gmail.com>
2022-03-03 14:05:25 -0600

    fixed no cache folder

--- python/__info__.py
+++ python/__info__.py
@@ -1,5 +1,5 @@
 def __module_version__():
-	return 0, 0, 54 
+	return 0, 0, 55 
 
 
 def __module_name__():
--- python/src/util.py
+++ python/src/util.py
@@ -4,17 +4,21 @@ import json
 global cellworld_data_base_uri
 from datetime import datetime, timedelta
 import os
+
 cellworld_data_base_uri = "https://raw.githubusercontent.com/germanespinosa/cellworld_data/master/"
 cellworld_cache_folder = os.environ.get("CELLWORLD_CACHE")
 
 
 def get_resource(resource_type: str, key0, *argv):
     resource_uri = cellworld_data_base_uri + resource_type + "/" + key0
-    file_path = cellworld_cache_folder + "/" + resource_type + "/" + key0
+    file_path = ""
+    if cellworld_cache_folder:
+        file_path = cellworld_cache_folder + "/" + resource_type + "/" + key0
     for arg in argv:
         resource_uri += "." + arg
-        file_path += "." + arg
-    if os.path.exists(file_path):
+        if cellworld_cache_folder:
+            file_path += "." + arg
+    if cellworld_cache_folder and os.path.exists(file_path):
         content = ""
         with open(file_path, "r") as text_file:
             content = text_file.read()
