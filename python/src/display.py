834c03193b9e14e6e7df859dc23ccd40e0a2f5ac
AlexT <alexanderlai2024@u.northwestern.edu>
2022-03-04 10:03:50 -0600

    added transparency option to circle and arrow

--- python/src/display.py
+++ python/src/display.py
@@ -96,14 +96,14 @@ class Display:
                     lcolor = color
                 self.ax.plot([x[i], x[i+1]], [y[i], y[i+1]], color=lcolor, alpha=.5, linewidth=3)
 
-    def circle(self, location: Location, radius: float, color):
-        circle_patch = plt.Circle((location.x, location.y), radius, color=color)
+    def circle(self, location: Location, radius: float, color, alpha: float = 1.0):
+        circle_patch = plt.Circle((location.x, location.y), radius, color=color, alpha=alpha)
         return self.ax.add_patch(circle_patch)
 
-    def arrow(self, beginning: Location, theta: float, dist: float, color, head_width: float = .02):
+    def arrow(self, beginning: Location, theta: float, dist: float, color, head_width: float = .02, alpha: float = 1.0):
         ending = beginning.copy().move(theta=theta, dist=dist)
         length = ending - beginning
-        return self.ax.arrow(beginning.x, beginning.y, length.x, length.y, color=color, head_width=head_width, length_includes_head=True)
+        return self.ax.arrow(beginning.x, beginning.y, length.x, length.y, color=color, head_width=head_width, length_includes_head=True, alpha=alpha)
 
     def agent(self, step: Step = None, agent_name: str = None, location: Location = None, rotation: float = None, color=None, size: float = 40.0, show_trajectory: bool = True, marker: Path=None):
         if step:
