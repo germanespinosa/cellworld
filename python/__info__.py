56df1a48b20a925ea529ad43263698d61ca4181f
German Espinosa <germanespinosa@gmail.com>
2022-06-15 11:13:32 -0500

    version 0.0.84

--- python/__info__.py
+++ python/__info__.py
@@ -1,5 +1,5 @@
 def __module_version__():
-	return 0, 0, 83 
+	return 0, 0, 85 
 
 
 def __module_name__():
--- python/setup/setup.py
+++ python/setup/setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='cellworld',description='Maciver Lab computational biology research package',author='german espinosa',author_email='germanespinosa@gmail.com',packages=['cellworld'],install_requires=['numpy', 'matplotlib', 'json-cpp', 'tcp-messages', 'networkx'],license='MIT',version='0.0.82',zip_safe=False)
+setup(name='cellworld',description='Maciver Lab computational biology research package',author='german espinosa',author_email='germanespinosa@gmail.com',packages=['cellworld'],install_requires=['numpy', 'matplotlib', 'json-cpp', 'tcp-messages', 'networkx'],license='MIT',version='0.0.84',zip_safe=False)
