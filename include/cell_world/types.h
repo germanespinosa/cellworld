75d9a51ec4802505f573d326f367826332a8be9f
German Espinosa <germanespinosa@gmail.com>
2022-11-17 14:45:39 -0600

    added math definition for windows build

--- include/cell_world/types.h
+++ include/cell_world/types.h
@@ -1,5 +1,12 @@
 #pragma once
 #include <json_cpp.h>
+#ifndef M_E
+# define M_E		2.7182818284590452354	/* e */
+#endif
+
+#ifndef M_PI
+# define M_PI		3.14159265358979323846	/* pi */
+#endif
 
 namespace cell_world {
     const int Not_found = -1;
